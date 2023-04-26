34a3f0fb16ebea0954a5410ba290556a4c286b58
Arvind <arvind.tangavelou@edu.esiee.fr>
2023-04-26 16:38:14 +0200

    3 API + début de style

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const searchInput = document.getElementById('search-input');
 const resultsList = document.getElementById('results-list');
+const resultsFrom = document.getElementById('results-from');
 
 searchInput.addEventListener('input', () => {
   console.log(searchInput.value);
 });
 
+function popularCity()
+{
+  const apiUrl = `https://api.comparatrip.eu/cities/popular/5`;
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(results => {
+        resultsList.innerHTML = '';
+        results.forEach(result => {
+          const option = document.createElement('option');
+          option.value = result.local_name;
+          option.text  = result.local_name;
+          resultsList.appendChild(option);
+        });
+      })
+      .catch(error => console.error(error));
+}
+
 searchInput.addEventListener('input', function() {
   const searchTerm = searchInput.value.trim();
-  if (searchTerm.length > 2) {
-    resultsList.innerHTML = '';
-    return;
-  }
-  
+  if (searchTerm.length < 1) 
+  {
+    popularCity();
+  }  
   const apiUrl = `https://api.comparatrip.eu/cities/autocomplete/?q=${searchTerm}`;
   
   fetch(apiUrl)
@@ -21,9 +38,26 @@ searchInput.addEventListener('input', function() {
       results.forEach(result => {
         const option = document.createElement('option');
         option.value = result.local_name;
+        option.text  = result.local_name;
         resultsList.appendChild(option);
-        console.log(option.value);
+      });
+    })
+    .catch(error => console.error(error));
+
+    const apiUrl2 = `https://api.comparatrip.eu/cities/popular/from/${searchTerm}/5`;
+  
+  fetch(apiUrl2)
+    .then(response => response.json())
+    .then(results => {
+      resultsFrom.innerHTML = '';
+      results.forEach(result => {
+        const option = document.createElement('option');
+        option.value = result.local_name;
+        option.text  = result.local_name;
+        resultsFrom.appendChild(option);
       });
     })
     .catch(error => console.error(error));
 });
+
+popularCity();
